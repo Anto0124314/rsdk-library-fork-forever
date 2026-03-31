3b7ad0557d2daa856d23b670d20e64fa38a08200
TheSmallAnto <antd39487@gmail.com>
2026-03-31 03:27:32 -0400

    Update global.d.ts

--- types/global.d.ts
+++ types/global.d.ts
@@ -4,11 +4,5 @@ declare global {
     interface Window {
         TS_InitFS: (p: string, f: () => void) => Promise<void>;
         __engineConsoleAppend: (text: string) => void;
-        Module?: {
-            print: (text: string) => void;
-            printErr: (text: string) => void;
-            setStatus: (text: string) => void;
-            canvas: HTMLCanvasElement;
-        };
     }
 }
